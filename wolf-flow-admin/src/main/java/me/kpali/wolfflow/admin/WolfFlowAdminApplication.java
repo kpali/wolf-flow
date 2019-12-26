d2fc696c64dcf4ec7d7db8eaa11df66509bb2a15
kpali <kpali@qq.com>
2019-12-26 20:08:02 +0800

    调整项目结构

---
+++ wolf-flow-admin/src/main/java/me/kpali/wolfflow/admin/WolfFlowAdminApplication.java
@@ -0,0 +1,13 @@
+package me.kpali.wolfflow.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WolfFlowAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WolfFlowAdminApplication.class, args);
+    }
+
+}
---
+++ wolf-flow-admin/src/test/java/me/kpali/wolfflow/admin/WolfFlowAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package me.kpali.wolfflow.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WolfFlowAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
