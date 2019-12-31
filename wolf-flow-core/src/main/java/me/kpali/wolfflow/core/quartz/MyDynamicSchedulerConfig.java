d66b137aee7ce56d6c509ac520d39047913690e6
kpali <kpali@qq.com>
2019-12-31 23:48:25 +0800

    nothing

--- wolf-flow-core/src/main/java/me/kpali/wolfflow/core/quartz/MyDynamicSchedulerConfig.java
+++ wolf-flow-core/src/main/java/me/kpali/wolfflow/core/quartz/MyDynamicSchedulerConfig.java
@@ -1,8 +1,6 @@
 package me.kpali.wolfflow.core.quartz;
 
 import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,27 +39,10 @@ public class MyDynamicSchedulerConfig {
             destroyMethod = "destroy"
     )
     public MyDynamicScheduler getMyDynamicScheduler(SchedulerFactoryBean schedulerFactory) {
-        StdSchedulerFactory
         Scheduler scheduler = schedulerFactory.getScheduler();
         MyDynamicScheduler myDynamicScheduler = new MyDynamicScheduler();
         myDynamicScheduler.setScheduler(scheduler);
         return myDynamicScheduler;
     }
-
-//    @Bean
-//    public StdSchedulerFactory getStdSchedulerFactory() {
-//        return new StdSchedulerFactory();
-//    }
-//
-//    @Bean(
-//            initMethod = "start",
-//            destroyMethod = "destroy"
-//    )
-//    public MyDynamicScheduler getMyDynamicScheduler(StdSchedulerFactory schedulerFactory) throws SchedulerException {
-//        Scheduler scheduler = schedulerFactory.getScheduler();
-//        MyDynamicScheduler myDynamicScheduler = new MyDynamicScheduler();
-//        myDynamicScheduler.setScheduler(scheduler);
-//        return myDynamicScheduler;
-//    }
 
 }
