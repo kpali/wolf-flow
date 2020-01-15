df366bcfee0b6bf448cc5248105b9f1956b112b6
kpali <kpali@qq.com>
2020-01-15 14:51:05 +0800

    nothing

---
+++ wolf-flow-core/src/main/java/me/kpali/wolfflow/core/model/ContextKey.java
@@ -0,0 +1,11 @@
+package me.kpali.wolfflow.core.model;
+
+/**
+ * 上下文键
+ *
+ * @author kpali
+ */
+public class ContextKey {
+    public static final String FROM_TASK_ID = "fromTaskId";
+    public static final String TO_TASK_ID = "toTaskId";
+}
--- wolf-flow-core/src/main/java/me/kpali/wolfflow/core/schedule/TaskFlowScheduler.java
+++ wolf-flow-core/src/main/java/me/kpali/wolfflow/core/schedule/TaskFlowScheduler.java
@@ -252,8 +252,8 @@ public class TaskFlowScheduler {
         // 任务流执行
         this.triggerThreadPool.execute(() -> {
             TaskFlowContext taskFlowContext = new TaskFlowContext();
-            taskFlowContext.put("fromTaskId", String.valueOf(fromTaskId));
-            taskFlowContext.put("toTaskId", String.valueOf(toTaskId));
+            taskFlowContext.put(ContextKey.FROM_TASK_ID, String.valueOf(fromTaskId));
+            taskFlowContext.put(ContextKey.TO_TASK_ID, String.valueOf(toTaskId));
             try {
                 // 任务流等待执行
                 this.publishTaskFlowStatusChangeEvent(finalTaskFlow, taskFlowContext, TaskFlowStatusEnum.WAIT_FOR_EXECUTE.getCode(), null);
