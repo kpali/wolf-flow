3dcfddf87bbea90fb9864a9f36a6594bca08c2a1
kpali <kpali@qq.com>
2020-01-29 23:56:14 +0800

    使用Redis实现集群控制器

---
+++ wolf-flow-sample/src/main/java/me/kpali/wolfflow/sample/config/RedissonSpringDataConfig.java
@@ -0,0 +1,26 @@
+package me.kpali.wolfflow.sample.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+@Configuration
+public class RedissonSpringDataConfig {
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson(@Value("classpath:/redisson.yaml") Resource configFile) throws IOException {
+        Config config = Config.fromYAML(configFile.getInputStream());
+        return Redisson.create(config);
+    }
+}
--- wolf-flow-sample/src/main/java/me/kpali/wolfflow/sample/taskflow/MyClusterController.java
+++ wolf-flow-sample/src/main/java/me/kpali/wolfflow/sample/taskflow/MyClusterController.java
@@ -1,10 +1,49 @@
 package me.kpali.wolfflow.sample.taskflow;
 
 import me.kpali.wolfflow.core.cluster.impl.DefaultClusterController;
+import me.kpali.wolfflow.core.model.TaskFlowExecRequest;
+import org.redisson.api.RLock;
+import org.redisson.api.RQueue;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Primary
 @Component
 public class MyClusterController extends DefaultClusterController {
+    private static final Logger log = LoggerFactory.getLogger(MyClusterController.class);
+
+    @Autowired
+    private RedissonClient redisson;
+
+    @Override
+    public boolean tryLock(String name) {
+        int lockLeaseTime = 60;
+        int lockWaitTime = 10;
+        RLock lock = redisson.getLock(name);
+        boolean res = false;
+        try {
+            res = lock.tryLock(lockWaitTime, lockLeaseTime, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            log.error("尝试获取分布式锁异常：" + e.getMessage(), e);
+        }
+        return res;
+    }
+
+    @Override
+    public boolean offer(TaskFlowExecRequest request) {
+        RQueue<TaskFlowExecRequest> queue = redisson.getQueue("taskFlowExecQueue");
+        return queue.offer(request);
+    }
+
+    @Override
+    public TaskFlowExecRequest poll() {
+        RQueue<TaskFlowExecRequest> queue = redisson.getQueue("taskFlowExecQueue");
+        return queue.poll();
+    }
 }
