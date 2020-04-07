b48db1c571bc0044cf81f272072680dad70d34fd
kpali <kpali@qq.com>
2020-04-07 17:49:16 +0800

    优化任务流上下文数据类型转换，防止因反序列化导致数据类型发生变化而无法取值的问题

--- wolf-flow-core/src/main/java/me/kpali/wolfflow/core/model/Context.java
+++ wolf-flow-core/src/main/java/me/kpali/wolfflow/core/model/Context.java
@@ -12,10 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Context extends ConcurrentHashMap<String, Object> {
     public <T> T getValue(String key, Class<T> clazz) {
         Object value = this.get(key);
-        if (value != null) {
-            return clazz.cast(value);
-        }
-        return null;
+        return this.cast(value, clazz);
     }
 
     public <T> List<T> getList(String key, Class<T> clazz) {
@@ -24,10 +21,24 @@ public class Context extends ConcurrentHashMap<String, Object> {
             List<T> result = new ArrayList<T>();
             if (value instanceof List<?>) {
                 for (Object o : (List<?>) value) {
-                    result.add(clazz.cast(o));
+                    result.add(this.cast(o, clazz));
                 }
                 return result;
             }
+        }
+        return null;
+    }
+
+    private <T> T cast(Object obj, Class<T> clazz) {
+        if (obj != null) {
+            if (!clazz.isInstance(obj) && obj instanceof Integer) {
+                // Integer类型 转 Long类型
+                Long longObj = ((Integer) obj).longValue();
+                if (clazz.isInstance(longObj)) {
+                    return clazz.cast(longObj);
+                }
+            }
+            return clazz.cast(obj);
         }
         return null;
     }
